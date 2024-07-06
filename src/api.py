ffe44393978f8c32c15b0ed3846dd4880aea23c6
Christophe Aballea <chr.aballea@gmail.com>
2024-07-06 17:37:10 +0200

    Classe Preprocessor

--- src/api.py
+++ src/api.py
@@ -7,6 +7,50 @@ import shap
 
 app = Flask(__name__)
 
+class Preprocessor(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.le_dict = {}
+        
+    def fit(self, X, y=None):
+        # Fit du label encoder pour les features catégorielles 
+        for col in X:
+            if X[col].dtype == 'object' and len(list(X[col].unique())) <= 2:
+                le = LabelEncoder()
+                le.fit(X[col])
+                self.le_dict[col] = le
+        
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        
+        # Label encoding
+        for col, le in self.le_dict.items():
+            X[col] = le.transform(X[col])
+        
+        # One-hot encoding
+        X = pd.get_dummies(X)
+        
+        # Alignement train et test
+        if hasattr(self, 'columns'):
+            X = X.reindex(columns=self.columns, fill_value=0)
+        else:
+            self.columns = X.columns
+        
+        # Dates en anomalies
+        X['DAYS_EMPLOYED_ANOM'] = X["DAYS_EMPLOYED"] == 365243
+        X['DAYS_EMPLOYED'] = X['DAYS_EMPLOYED'].replace({365243: np.nan})
+        X['DAYS_EMPLOYED'] = abs(X['DAYS_EMPLOYED'])
+        X['DAYS_BIRTH'] = abs(X['DAYS_BIRTH'])
+        
+        # Feature engineering
+        X['CREDIT_INCOME_PERCENT'] = X['AMT_CREDIT'] / X['AMT_INCOME_TOTAL']
+        X['ANNUITY_INCOME_PERCENT'] = X['AMT_ANNUITY'] / X['AMT_INCOME_TOTAL']
+        X['CREDIT_TERM'] = X['AMT_ANNUITY'] / X['AMT_CREDIT']
+        X['DAYS_EMPLOYED_PERCENT'] = X['DAYS_EMPLOYED'] / X['DAYS_BIRTH']
+        
+        return X
+
 # Emplacement des fichiers
 base_dir = os.path.dirname(os.path.abspath(__file__))
 preprocessor_path = os.path.join(base_dir, '..', 'data', 'processed', 'preprocessor.pkl')
